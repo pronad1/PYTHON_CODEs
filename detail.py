356bdeddc9354d9bd90a901ce8a6b47515500036
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-11-11 10:17:22 +0600

    11/11/24

--- detail.py
+++ detail.py
@@ -1,5 +1,4 @@
-x=4;
-print(x);
+
 x="edd";
 print(x);
 MyVariableName = "John"; #Pascal Case(Each word starts with a capital letter)
@@ -9,3 +8,8 @@ print(my_variable_name);
 x, y, z = "Orange", "Banana", "Cherry"; #Unpack a list:
 
 print(x,' ',y," ",z,'\n');
+print(x+y+z,'\n');
+print(x,y,z,'\n');
+x=4
+y=5
+print(x+y,'\n');
