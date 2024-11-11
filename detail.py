e5077e22c36f5d0d0a845dd6e56f2ef33af36254
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-11-11 10:14:31 +0600

    11/11/24

--- detail.py
+++ detail.py
@@ -6,6 +6,6 @@ MyVariableName = "John"; #Pascal Case(Each word starts with a capital letter)
 print(MyVariableName); 
 my_variable_name = "John"; #Snake Case(Each word is separated by an underscore character)
 print(my_variable_name);
-x, y, z = "Orange", "Banana", "Cherry";
+x, y, z = "Orange", "Banana", "Cherry"; #Unpack a list:
 
 print(x,' ',y," ",z,'\n');
