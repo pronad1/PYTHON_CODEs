f32c830ac353c6ccf631ec9eee24a899920ecbfb
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-11-12 12:05:18 +0600

    11/12/24

---
+++ string_fun.py
@@ -0,0 +1,9 @@
+txt="the things in the life are free!"
+print("free" in txt); print("Pro" in txt)
+if "prosenjit" not in txt:
+    print("NO, 'Prosenjit' is not present.")
+
+t="Prosenjit , the things in the life are free!"
+if "Prosenjit" in t:
+    print("YES, 'Prosennjit' is present")
+print("The length of the text is : ",len(t))
