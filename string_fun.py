7ecfe720f69b15f88e04e3395c222801bc72e46a
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-11-12 13:49:47 +0600

    11/12/24

--- string_fun.py
+++ string_fun.py
@@ -7,3 +7,16 @@ t="Prosenjit , the things in the life are free!"
 if "Prosenjit" in t:
     print("YES, 'Prosennjit' is present")
 print("The length of the text is : ",len(t))
+
+# Slicing "We can return a range of characters by using the slice syntax "
+
+print(t[3:9])
+print(t[-5:-2])
+
+#Modify String
+print(t.upper())
+print(t.lower())
+
+# strip() "The strip() method removes any whitespace from the begginning or the end ";
+a=" Hey Man! "
+print(a.strip(),1)
