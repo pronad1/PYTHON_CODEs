7d06a53b8552c968a63bb4b555913fff719ee44d
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-11-15 02:58:05 +0600

    11/14/24

--- circuit.py
+++ circuit.py
@@ -1,16 +1,14 @@
-t=int(input())
-for i in range(t):
-    l=int(input())
-    z=0;o=0
-    for j in range(2*l):
-        x=int(input())
-        if x==0:
-            z+=1
-        else:
-            o+=1
-    if z==o:
-        print(1,' ',l,"\n")
-    elif z==0 or o==0:
-        print(0,' ',0,"\n")
-    else:
-        print(0,' ',o,"\n")
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    switches = list(map(int, input().split()))
+    
+    # Count the number of "on" switches
+    o = switches.count(1)
+    
+    # Calculate minimum and maximum lights that can be on
+    min_lights_on = max(0, o - n)
+    max_lights_on = min(o, n)
+    
+    # Output the results
+    print(min_lights_on, max_lights_on)
