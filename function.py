c8dd736f631f9edfcdf173f5723f195672c8bb84
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-11-15 15:17:08 +0600

    11/15/24

--- function.py
+++ function.py
@@ -7,3 +7,12 @@ for _ in range(5):
     my_function(input())
 
 my_function()
+
+def myfun(n):
+    return lambda a : a*n
+
+mydouble=myfun(2)
+mytriple=myfun(3)
+
+print(mydouble(11))
+print(mytriple(11))
