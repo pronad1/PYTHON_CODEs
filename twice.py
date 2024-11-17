a0c02ca8546e8551c65a85395aa23cc0da301f9f
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-11-18 01:34:50 +0600

    11/18/24

--- twice.py
+++ twice.py
@@ -2,4 +2,8 @@ for _ in range(int(input())):
     n=int(input())
     ans=0
     a=list(map(int,input().split()))
-    print(a)
+    ase=list(set(a))
+    for i in ase:
+        ans+=a.count(i)//2
+        
+    print(ans)
