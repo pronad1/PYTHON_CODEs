36dec551a8c85434d1cfbfc66a67f80e7b16a37a
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-11-23 20:57:56 +0600

    11/23/24

--- shohag_mod.py
+++ shohag_mod.py
@@ -2,8 +2,8 @@
 for _ in range(int(input())):
     n = int(input())
     sequence=[]
-    base=2
+    base=1
     for i in range(1, n + 1):
         sequence.append(base)
-        base+=1
+        base+=i*2
     print(" ".join(map(str, sequence)))
