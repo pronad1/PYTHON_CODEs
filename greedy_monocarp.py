4dd7b17917098a5065c15f385661977e0a4d41cc
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-12-02 22:12:57 +0600

    11/30/24

--- greedy_monocarp.py
+++ greedy_monocarp.py
@@ -2,5 +2,16 @@ for _ in range(int(input())):
     n,k = map(int,input().split())
     arr=list(map(int,input().split()))
     arr.sort(reverse=True)
-    for i in range(n):
-        print(arr[i],end=" ")
+    total=sum(arr)
+    if total<k:
+        print(k-total)
+    else:
+        cu=0
+        for i in range(n):
+            cu+=arr[i]
+            if cu>=k:
+                if cu>k:
+                    cu-=arr[i]
+
+                break
+        print(k-cu)
