707a5fc30e4d2b7456c46572c2d522d23c5fa102
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-12-03 18:53:28 +0600

    11/3/24

--- alyona_jigsaw_puzzle.py
+++ alyona_jigsaw_puzzle.py
@@ -1,4 +1,11 @@
 for _ in range(int(input())):
     n=int(input())
     arr=list(map(int,input().split()))
-    c=0
+    c=1
+    s=-1
+    d=8
+    for i in range(n):
+        s+=arr[i]
+        if int(s/d)>=1:
+            c+=1
+            d+=
