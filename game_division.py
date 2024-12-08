3e1737824f8eb5919e124104ca9d20278fb0b41a
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-12-08 20:53:25 +0600

    12/8/24

--- game_division.py
+++ game_division.py
@@ -1,19 +1,21 @@
-for _ in range(int(input())):
-    n,k=map(int,input().split())
-    a=list(map(int,input().split()))
-    f=False
+t = int(input())
+
+for _ in range(t):
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    possible = False
+
     for i in range(n):
-        cf=False
+        can_win = False
         for j in range(n):
-            if i != j and abs(a[i]-a[j])%k !=0:
-                cf=True
+            if i != j and abs(a[i] - a[j]) % k != 0:
+                can_win = True
                 break
-        if cf:
-            f=True
+        if can_win:
+            possible = True
             print("YES")
-            print(i+1)
+            print(i + 1)
             break
 
-
-    if not f:
+    if not possible:
         print("NO")
