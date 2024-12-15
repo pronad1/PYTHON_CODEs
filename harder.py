b771e4cf7b0b010ebe9006f3dc568ec7a223f762
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-12-15 21:29:29 +0600

    12/15/24

--- harder.py
+++ harder.py
@@ -1,18 +1,26 @@
+t = int(input())
 
-for _ in range(int(input())):
-
+for _ in range(t):
     n = int(input())
     a = list(map(int, input().split()))
 
     freq = [0] * (n + 1)
     b = [0] * n
 
-    for i in range(n):
-        ai = a[i]
+    b[n - 1] = n
+    freq[n] += 1
+    max_freq = 1
 
-        freq[ai] += 1
-        b[i] = ai
+    for i in range(n - 2, -1, -1):
+        if freq[a[i]] == max_freq:
+            b[i] = a[i]
+            freq[a[i]] += 1
+        else:
+            for j in range(1, n + 1):
+                if freq[j] < max_freq:
+                    b[i] = j
+                    freq[j] += 1
+                    break
+            max_freq += 1
 
-    print(" ".join(map(str, b)))
-
-
+    print(*b)
