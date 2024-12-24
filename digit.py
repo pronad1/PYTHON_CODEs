b42e82cb08c0cc1899d5fe5f1b58e747f100b9fd
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-12-25 00:22:12 +0600

    12/25/24

--- digit.py
+++ digit.py
@@ -8,3 +8,12 @@ for _ in range(int(input())):
     if n>=3 or d==7:
         a.append(7)
     b=0
+
+    if n>=6:
+        b=9
+    elif n>=3:
+        b=3
+    else:
+        b=1
+    if(b*d)%9==0:
+        a.append(9)
