73b29d412e456f2a81f89bb211e76fb0c2872e93
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-12-25 00:20:35 +0600

    12/25/24

--- digit.py
+++ digit.py
@@ -1,2 +1,10 @@
 for _ in range(int(input())):
     n,d=map(int,input().split())
+    a=[1]
+    if n>=3 or d%3==0:
+        a.append(3)
+    if d==5:
+        a.append(5)
+    if n>=3 or d==7:
+        a.append(7)
+    b=0
