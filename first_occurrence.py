5fed9dde8d1c83c4378d94bee723e8d111844a7a
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-13 18:16:17 +0600

    2/13/25

--- first_occurrence.py
+++ first_occurrence.py
@@ -1,8 +1,11 @@
 for i in range(int(input())):
 
-    haystack,needle=map(int,input().split())
+    k=False
+    haystack,needle=map(str,input().split())
     for i in range(len(haystack)-len(needle)+1):
         if haystack[i:i+len(needle)]==needle:
             print(i)
+            k=True
     
-    print(-1)
+    if k==False:
+        print(-1)
