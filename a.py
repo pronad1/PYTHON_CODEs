61e30de0414e0b4b44358238452f1de283705bf4
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-18 20:43:27 +0600

    2/18/25

--- a.py
+++ a.py
@@ -1,12 +1,29 @@
-t = int(input())  
+t = int(input())
 for _ in range(t):
-    n = int(input())  
-    b = list(map(int, input().split()))  
-
-    valid = True
-    for i in range(1, len(b)):  
-        if b[i] == 1 and b[i-1] == 0:
-            valid = False
+    n = int(input())
+    b = list(map(int, input().split()))
+    m = n - 2
+    possible = False
+    for initial in [0, 1]:
+        current = {initial}
+        valid = True
+        for i in range(m):
+            next_current = set()
+            for state in current:
+                if b[i] == 1:
+                    if state != 1:
+                        continue
+                    next_current.add(1)
+                else:
+                    if state == 0:
+                        next_current.add(0)
+                        next_current.add(1)
+                    else:
+                        next_current.add(0)
+            current = next_current
+            if not current:
+                break
+        if current:
+            possible = True
             break
-
-    print("YES" if valid else "NO")
+    print("YES" if possible else "NO")
