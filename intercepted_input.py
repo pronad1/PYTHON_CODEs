30a352aae8f556ff3ab9e6a0f0afa175ce158038
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-23 23:55:40 +0600

    2/23/25

--- intercepted_input.py
+++ intercepted_input.py
@@ -6,4 +6,6 @@ for i in range(int(input())):
     for i in range(n-1):
         if r%a[i]==0:
             for j in range(i+1,n):
-            
+                if a[i]*a[j]==r:
+                    print(a[i],a[j])
+                    break
