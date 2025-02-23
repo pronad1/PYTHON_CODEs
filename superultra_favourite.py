b0bec36de82a8b3c2eba33840d52c4db0d44225b
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-24 00:32:00 +0600

    2/24/25

--- superultra_favourite.py
+++ superultra_favourite.py
@@ -1,3 +1,12 @@
 for i in range(int(input())):
     n=int(input())
-    
+    if n<5:
+        print(-1)
+        continue
+
+    odd=list(range(1,n+1,2))
+    even=list(range(2,n+1,2))
+    odd.remove(5)
+    even.remove(4)
+
+    print(*(odd+[5]+[4]+even))
