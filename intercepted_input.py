e92201c0acb641b301b92bb5ff78ea5fc8b58182
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-24 00:00:21 +0600

    2/24/25

--- intercepted_input.py
+++ intercepted_input.py
@@ -3,7 +3,6 @@ for i in range(int(input())):
     a=list(map(int,input().split()))
     r=n-2
     i=0
-    sorted(a)
     for i in range(n-1):
         if r%a[i]==0:
             for j in range(i+1,n):
