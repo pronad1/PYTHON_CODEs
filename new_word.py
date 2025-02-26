19703392486d946c7bf7ec79b8dc49f23e73bf42
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-26 09:16:00 +0600

    2/26/25

--- new_word.py
+++ new_word.py
@@ -1,6 +1,13 @@
+from math import ceil
+
 for i in range(int(input())):
     n,k,p=map(int,input().split())
     if k==0:
         print(0)
         continue
-    
+    k=k/p
+    r=ceil(k)
+    if r>n:
+        print(-1)
+    else:
+        print(r)
