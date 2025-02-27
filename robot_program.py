c97299237d037dfe2158ba0af7d6cfe9022752ab
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-27 21:51:45 +0600

    2/27/25

--- robot_program.py
+++ robot_program.py
@@ -1,4 +1,27 @@
 for i in range(int(input())):
     n,x,k=map(int,input().split())
     s=str(input())
-    
+    pos=x
+    if x==0 :
+        print(k)
+        continue
+    seq=[x]
+    for i in s:
+        if i=='R':
+            pos+=1
+        else:
+            pos-=1
+        seq.append(pos)
+
+    zero=seq.count(0)
+
+    if zero==0:
+        print(0)
+        continue
+
+    ful=k//n
+    rem=k%n
+    add=seq[:rem].count(0)
+
+    r=ful*zero+add
+    print(r)
