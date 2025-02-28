7046a2d772e0d24a732cf1d84a42756782b009fb
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-28 21:54:49 +0600

    2/28/25

--- perfecto.py
+++ perfecto.py
@@ -1,38 +1,30 @@
-import math
+import sys
 
-def is_square(x):
-    if x < 0:
-        return False
-    s = math.isqrt(x)
-    return s * s == x
+input = sys.stdin.read
+data = input().split()
 
-def main():
-    import sys
-    input = sys.stdin.read().split()
-    t = int(input[0])
-    idx = 1
-    for _ in range(t):
-        n = int(input[idx])
-        idx += 1
-        sum_total = n * (n + 1) // 2
-        if is_square(sum_total):
-            print(-1)
-            continue
-        
-        perm = list(range(n, 0, -1))
-        square_idx = -1
-        current_sum = 0
-        for i in range(n):
-            current_sum += perm[i]
-            if is_square(current_sum):
-                square_idx = i
-                break
-        
-        if square_idx != -1:
-            if square_idx < n - 1:
-                perm[square_idx], perm[square_idx + 1] = perm[square_idx + 1], perm[square_idx]
-        
-        print(' '.join(map(str, perm)))
+t = int(data[0])
+results = []
+index = 1
 
-if __name__ == "__main__":
-    main()
+for _ in range(t):
+    n = int(data[index])
+    index += 1
+
+    if n == 1:
+        results.append("-1")
+        continue
+
+    p = list(range(1, n + 1))
+
+    if n % 2 == 0:
+        for i in range(0, n, 2):
+            p[i], p[i + 1] = p[i + 1], p[i]
+    else:
+        p[0] = n
+        for i in range(1, n - 1, 2):
+            p[i], p[i + 1] = p[i + 1], p[i]
+
+    results.append(" ".join(map(str, p)))
+
+sys.stdout.write("\n".join(results) + "\n")
