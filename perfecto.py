6f078d3cf182a146a37634586954c3c90d9a8564
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-03-02 17:40:30 +0600

    3/2/25

--- perfecto.py
+++ perfecto.py
@@ -1,9 +1,20 @@
 import math
 
-for i in range(int(input())):
+for _ in range(int(input())):
     n=int(input())
 
     if math.sqrt(n*(n+1)/2)%1 ==0:
         print(-1)
     else:
+        p=[1]
+        prifix=0
+        for i in range(n-1):
+            prifix +=i+1
+            if math.sqrt(prifix)%1==0:
+                p.pop()
+                p.append(i+2)
+                p.append(i+1)
+            else:
+                p.append(i+2)
         
+        print(*p)
