6008197895faf4a7f44719d703c1c0d5f9455bc5
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-03-11 21:46:22 +0600

    3/11/25

--- xor_triangle.py
+++ xor_triangle.py
@@ -14,3 +14,16 @@
 #     if not f:
 #         print(-1)
 
+t = int(input())
+
+for _ in range(t):
+    x = int(input())
+    
+    if (x & (x - 1)) == 0 or ((x + 1) & x) == 0:
+        print(-1)
+    else:
+        lsb = x & -x
+        k = lsb.bit_length() - 1
+        mask = (x + 1) & (~x)
+        m = mask.bit_length() - 1
+        print((1 << k) + (1 << m))
