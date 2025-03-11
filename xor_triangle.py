cf00b882eea730fb424ec6ca18552ae8520bd134
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-03-11 21:28:37 +0600

    3/11/25

--- xor_triangle.py
+++ xor_triangle.py
@@ -1,15 +1,27 @@
+# t = int(input())
+
+# for _ in range(t):
+#     x = int(input())
+#     f= False
+
+#     for y in range(x - 1, 0, -1):
+#         z = x ^ y
+#         if x + y > z and x + z > y and y + z > x:
+#             print(y)
+#             f= True
+#             break
+    
+#     if not f:
+#         print(-1)
+
+
 t = int(input())
 
 for _ in range(t):
     x = int(input())
-    f= False
-
-    for y in range(x - 1, 0, -1):
-        z = x ^ y
-        if x + y > z and x + z > y and y + z > x:
-            print(y)
-            f= True
-            break
+    y = x & -x  # Lowest power of 2 in x
     
-    if not f:
+    if y < x:
+        print(y)
+    else:
         print(-1)
