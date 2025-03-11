535453e3ee8a73fde2d0610ef69d13049708e24a
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-03-11 21:19:13 +0600

    3/11/25

--- third.py
+++ third.py
@@ -1,13 +1,10 @@
-t = int(input().strip())
-results = []
+import sys
 
+t = int(sys.stdin.readline())
 for _ in range(t):
-    n = int(input().strip())
-    arr = list(map(int, input().split()))
+    n = int(sys.stdin.readline())
+    a = list(map(int, sys.stdin.readline().split()))
     if n == 1:
-        results.append(str(arr[0]))
+        print(a[0])
     else:
-        arr.sort()
-        results.append(str(arr[-1] + arr[-2]))
-
-print("\n".join(results))
+        print(sum(a) - (n - 1))
