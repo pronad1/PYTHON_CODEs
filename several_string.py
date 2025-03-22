905d9b8c1e91bf0813830e278dc6412412b87470
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-03-22 21:58:32 +0600

    3/22/25

--- several_string.py
+++ several_string.py
@@ -2,9 +2,14 @@ for _ in range(int(input())):
     n,k=map(int,input().split())
     s=str(input())
 
+    if s < s[::-1]:
+        print("YES")
+        continue
+    
     if len(set(s)) == 1:
         print("NO")
         continue
+
     if k==0:
         if s[0]<s[n-1]:
             print("YES")
@@ -12,4 +17,11 @@ for _ in range(int(input())):
             print("NO")
         continue
     
+    if k==1:
+        c=min(s)
+        if s[n-1]==c and s[0]!=s[n-1]:
+            print("NO")
+        else:
+            print("YES")
+        continue
     print("YES")
