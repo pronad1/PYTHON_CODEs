adb4a9477474d2b3a63bd84d55d51783e7db98bb
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-03-22 21:13:05 +0600

    3/22/25

--- several_string.py
+++ several_string.py
@@ -1,4 +1,47 @@
-for _ in range(int(input())):
-    n,k=map(int,input().split())
-    s = input().strip()
-    
+def is_universal(s):
+    rev = s[::-1]
+    return s < rev
+
+def solve():
+    import sys
+    input = sys.stdin.read().split()
+    idx = 0
+    t = int(input[idx])
+    idx += 1
+    for _ in range(t):
+        n, k = int(input[idx]), int(input[idx + 1])
+        idx += 2
+        s = input[idx]
+        idx += 1
+        if is_universal(s):
+            print("YES")
+            continue
+        possible = False
+        max_i = (n - 1) // 2
+        from collections import defaultdict
+        for i in range(max_i + 1):
+            cnt = defaultdict(int)
+            for c in s:
+                cnt[c] += 1
+            total_pairs = sum(v // 2 for v in cnt.values())
+            if total_pairs < i:
+                continue
+            remaining = n - 2 * i
+            if remaining < 2:
+                continue
+            unique = len(set(s))
+            if unique < 2:
+                continue
+            if remaining >= 2 and unique >= 2:
+                possible = True
+                if k >= (n-1):
+                    possible = True
+                else:
+                    possible = True
+                break
+        if possible:
+            print("YES")
+        else:
+            print("NO")
+
+solve()
