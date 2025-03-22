72bb1dff959f43fcc518b3335d02612a1cea9345
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-03-22 20:40:16 +0600

    3/22/25

--- several_string.py
+++ several_string.py
@@ -1,15 +1,4 @@
-t = int(input().strip())
-for _ in range(t):
-    n, k = map(int, input().split())
-    s = input().strip()
+for _ in range(int(input())):
+    n,k=map(int,input().split())
+    s=str(input())
     
-    if s < s[::-1]:
-        print("YES")
-        continue
-    
-    diff_count = sum(1 for i in range(n // 2) if s[i] != s[n - 1 - i])
-    
-    if diff_count <= k:
-        print("YES")
-    else:
-        print("NO")
