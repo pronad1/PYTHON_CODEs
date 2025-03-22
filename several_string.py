ceaa1db9d3436b9c77af2dbe17fc29d66e9f4829
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-03-22 21:30:18 +0600

    3/22/25

---
+++ several_string.py
@@ -0,0 +1,15 @@
+for _ in range(int(input())):
+    n,k=map(int,input().split())
+    s=str(input())
+
+    if len(set(s)) == 1:
+        print("NO")
+        continue
+    if k==0:
+        if s[0]<s[n-1]:
+            print("YES")
+        else:
+            print("NO")
+        continue
+    
+    print("YES")
