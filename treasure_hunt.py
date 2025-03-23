d410974b637def7d464fbf514dd95faa90a3e3ce
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-03-23 22:40:26 +0600

    3/25/25

--- treasure_hunt.py
+++ treasure_hunt.py
@@ -1,12 +1,9 @@
 for _ in range(int(input())):
     x,y,a=map(int,input().split())
-    d=a*5
+    a+=0.5
     s=x+y
-    m=d%s
-    if m==0:
-        print("YEs")
-    else:
-        if m>x:
-            print("YES")
-        else:   
-            print("NO")
+    m=a%s
+    if m>x:
+        print("YES")
+    else:   
+        print("NO")
