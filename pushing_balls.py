93f56a35c136d3e4681bc9c978ae1f7a087ca48b
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-03-24 22:35:37 +0600

    3/25/25

--- pushing_balls.py
+++ pushing_balls.py
@@ -1,5 +1,24 @@
-for _ in range(int(input())):
-    n,m=map(int,input().split())
-    arr=[]
+def poss():
+    n, m = map(int, input().split())
+    mat = []
     for i in range(n):
-        arr.append(list(map(int,input().split())))
+        mat.append(list(map(int, input())))
+ 
+    r = [0] * n
+    c = [0] * m
+ 
+ 
+    for i in range(n):
+        for j in range(m):
+            if mat[i][j] == 1:
+                if not(c[j] == i or r[i] == j): return 'NO'
+                if c[j] == i:
+                    c[j] += 1
+ 
+                if r[i] == j:
+                    r[i] += 1
+ 
+    return 'YES'
+ 
+for _ in range(int(input())):
+    print(poss())
