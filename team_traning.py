94fa6bd5d580d32ee23417e79e334274f39bc2de
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-03-25 21:01:27 +0600

    3/25/25

---
+++ team_traning.py
@@ -0,0 +1,22 @@
+for _ in range(int(input())):
+    n, x = map(int, input().split())
+    a = list(map(int, input().split()))
+    a.sort()
+    
+    current_end = n - 1
+    count = 0
+    i = current_end
+    
+    while i >= 0:
+        ai = a[i]
+        k = (x + ai - 1) // ai
+        available = current_end - i + 1
+        
+        if available >= k:
+            count += 1
+            current_end = i - 1
+            i = current_end
+        else:
+            i -= 1
+    
+    print(count)
