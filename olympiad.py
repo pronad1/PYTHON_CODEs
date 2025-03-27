bd92986e3fd257221f07a3d0c9109e5cc23e139d
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-03-27 09:44:46 +0600

    3/26/25

--- olympiad.py
+++ olympiad.py
@@ -1,5 +1,5 @@
-t = int(input())
-for _ in range(t):
+
+for _ in range(int(input())):
     n = int(input())
     a = list(map(int, input().split()))
     counts = {0: 0, 1: 0, 2: 0, 3: 0, 5: 0}
@@ -9,7 +9,7 @@ for _ in range(t):
         num = a[i]
         if num in counts:
             counts[num] += 1
-            
+
         # Check if all required digits are satisfied
         if all(counts[d] >= required[d] for d in required):
             print(i + 1)
