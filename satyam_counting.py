fc3f900f84740f75ee261fead78da1f1c960d72d
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-03-27 09:54:06 +0600

    3/26/25

---
+++ satyam_counting.py
@@ -0,0 +1,19 @@
+for __ in range(int(input())):
+    n = int(input())
+    a = []
+    ans = 0
+
+    for _ in range(n):
+        x,y = map(int, input().split())
+        a.append((x, y))
+    
+    c = set(a)
+    # a.sort()
+    for i in range(1, n):
+        if a[i][0] == a[i - 1][0]:
+            ans += n - 2
+    for i in c:
+        if (i[0] - 1, i[1]^1) in c and (i[0] + 1, i[1]^1) in c:
+            ans += 1
+    
+    print(ans)
