de727b4d5112568c2df604068bbe8efee7a315b0
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-03-28 09:43:34 +0600

    3/28/25

--- final_verdict.py
+++ final_verdict.py
@@ -2,8 +2,9 @@ for _ in range(int(input())):
     n,x=map(int,input().split())
     a=map(int,input().split())
     s=sum(a)
-    
+
     if s==x*n:
         print("YES")
+        
     else:
         print("NO")
