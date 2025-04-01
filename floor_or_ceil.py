3d2aea7137a3f67dc72b479e1daad80b1a51ee0e
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-04-01 13:19:06 +0600

    4/1/25

--- floor_or_ceil.py
+++ floor_or_ceil.py
@@ -1,4 +1,19 @@
+def F(x, n):
+    while n:
+        if x == 0:
+            return x
+        x //= 2
+        n -= 1
+    return x
+
+def C(x, n):
+    while n:
+        if x <= 1:
+            return x
+        x = (x + 1) // 2
+        n -= 1
+    return x
+
 for _ in range(int(input())):
     x,n,m=map(int, input().split())
     print(F(C(x, m), n), C(F(x, n), m))
-    
