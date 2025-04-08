641b12a9fe3a41e384985310bb46ccc29ff2795f
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-04-08 21:38:31 +0600

    4/8/25

--- expensive_number.py
+++ expensive_number.py
@@ -1,8 +1,19 @@
 for _ in range(int(input())):
     s=str(input())
+    c=-1
+    pre=-1
     n=len(s)
     z=s.count('0')
     if z==0:
         print(n-1)
+    
+    
     else:
-        print(z)
+        for ch in reversed(s):
+            c+=1
+            if ch !='0':
+                break
+        print(n-1+c-z)
+
+
+        
