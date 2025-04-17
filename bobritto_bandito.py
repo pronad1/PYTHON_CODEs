b7e1bb0d7961abab05a41942e1757cecff77b93e
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-04-17 16:43:48 +0600

    4/17/25

--- bobritto_bandito.py
+++ bobritto_bandito.py
@@ -1,6 +1,12 @@
 for _ in range(int(input())):
     n,m,l,r=map(int,input().split())
     x=abs(l)
-    y=m-x
-    
-    print(l,"",y)
+    d=n-m
+    if x>=d:
+        x-=d
+        d=0
+    else:
+        d-=x
+        x=0
+
+    print(-x,"",r-d)
