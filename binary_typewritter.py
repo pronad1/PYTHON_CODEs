36a9e47e3fecee3ffdcbd4d6749e6dc7b0328d62
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-04-22 18:45:15 +0600

    4/22/25

--- binary_typewritter.py
+++ binary_typewritter.py
@@ -1,4 +1,10 @@
 for _ in range(int(input())):
     n = int(input())
     s =input().strip()
-    
+    cnt=0
+    for i in range(n-1):
+        if s[i]!= s[i+1]:
+            cnt+=1
+
+    if cnt==0:
+        print(n+int(s[0]=="1"))
