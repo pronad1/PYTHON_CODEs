d7c3b0064d286046e3b447d771d52fd2586a49e6
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-04-24 21:08:34 +0600

    4/24/25

--- cherry_bomb.py
+++ cherry_bomb.py
@@ -6,7 +6,7 @@ for _ in range(t):
 
     x_known = -1
     valid = True
-    min_x, max_x = 0, 10**18  # wide bounds for x
+    min_x, max_x = 0, 10**18 
 
     for i in range(n):
         if b[i] != -1:
@@ -19,7 +19,6 @@ for _ in range(t):
 
     for i in range(n):
         if b[i] == -1:
-            # b[i] ∈ [0, k] ⇒ x = a[i] + b[i] ⇒ x ∈ [a[i], a[i] + k]
             min_x = max(min_x, a[i])
             max_x = min(max_x, a[i] + k)
 
@@ -27,11 +26,9 @@ for _ in range(t):
         print(0)
     else:
         if x_known != -1:
-            # check if x_known is within valid range
             if min_x <= x_known <= max_x:
                 print(1)
             else:
                 print(0)
         else:
-            # all b[i] are -1, return number of integers in [min_x, max_x]
             print(max(0, max_x - min_x + 1))
