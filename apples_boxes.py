c09ccee0a6c99b33c5edd7afc08781c100bf5e4c
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-05-06 00:19:11 +0600

    5/6/25

--- apples_boxes.py
+++ apples_boxes.py
@@ -1,4 +1,12 @@
 for _ in range(int(input())):
     n, k = map(int, input().split())
     a = list(map(int, input().split()))
-    
+    mx= max(a)
+    mn= min(a)
+    if mx - mn > k:
+        print("Jerry")
+    else:
+        if sum(a) % 2 == 0:
+            print("Tom")
+        else:
+            print("Jerry")
