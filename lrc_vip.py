fa7564388d88af0b084b845492195de4f2c96496
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-05-05 23:35:26 +0600

    5/525

--- lrc_vip.py
+++ lrc_vip.py
@@ -6,4 +6,11 @@ for _ in range(int(input())):
     if al==1:
         print("NO")
     else:
-        
+        t=0
+        print("YES")
+        for i in range(n):
+            if a[i]!=1 and t==1:
+                print(1)
+                t=1
+            else:
+                print(2)
