eefd1b76a0b7311f0ad2ee7576a7f1bea8226366
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-05-06 00:29:48 +0600

    5/6/25

--- apples_boxes.py
+++ apples_boxes.py
@@ -1,12 +1,24 @@
-for _ in range(int(input())):
+def apples_in_boxes(test_cases):
+    results = []
+    for n, k, a in test_cases:
+        min_a = min(a)
+        limit = min_a + k
+        total = sum(min(ai, limit) for ai in a)
+        if total % 2 == 1:
+            results.append("Tom")
+        else:
+            results.append("Jerry")
+    return results
+
+# Input reading
+t = int(input())
+test_cases = []
+for _ in range(t):
     n, k = map(int, input().split())
     a = list(map(int, input().split()))
-    mx= max(a)
-    mn= min(a)
-    if mx - mn > k:
-        print("Jerry")
-    else:
-        if sum(a) % 2 ==1:
-            print("Tom")
-        else:
-            print("Jerry")
+    test_cases.append((n, k, a))
+
+# Solve and print results
+results = apples_in_boxes(test_cases)
+for res in results:
+    print(res)
