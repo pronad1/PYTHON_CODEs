29056ca489bb80dd45a11597f1017d4f780c24dd
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-05-19 18:14:14 +0600

    5/19/25

--- it's_time_duel.py
+++ it's_time_duel.py
@@ -3,7 +3,7 @@ for _ in range(int(input())):
     a = list(map(int, input().split()))
     ze = a.count(0)
     on = a.count(1)
-    if ze==on or ze==n or on==n:
+    if (ze==on and ze!=1) or ze==n or on==n:
         print("YES")
     else:
         print("NO") 
