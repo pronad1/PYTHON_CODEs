36bb22f327244ff12e18c9d5118fc8aca82a03d3
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-05-23 17:02:06 +0600

    5/23/25

--- farmer_john_card.py
+++ farmer_john_card.py
@@ -1,6 +1,15 @@
-for _ in range(int(input())):
-    n,m= map(int, input().split())
-    a = list(map(int, input().split()))
-    b = list(map(int, input().split()))
-
-    
+t = int(input())
+for _ in range(t):
+    n, m = map(int, input().split())
+    cards = [list(map(int, input().split())) for _ in range(n)]
+    for cow in range(n):
+        cards[cow].sort()
+    rounds = [[cards[cow][r] for cow in range(n)] for r in range(m)]
+    orders = []
+    for r in range(m):
+        order = sorted(range(n), key=lambda i: rounds[r][i])
+        orders.append(order)
+    if all(orders[r] == orders[0] for r in range(m)):
+        print(' '.join(str(i + 1) for i in orders[0]))
+    else:
+        print(-1)
