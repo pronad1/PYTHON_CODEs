291d20f7597bfef500b7300b9a40e6e454de3b50
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-05-23 17:06:04 +0600

    2/23/25

--- farmer_john_card.py
+++ farmer_john_card.py
@@ -1,15 +1,46 @@
-t = int(input())
-for _ in range(t):
-    n, m = map(int, input().split())
-    cards = [list(map(int, input().split())) for _ in range(n)]
-    for cow in range(n):
-        cards[cow].sort()
-    rounds = [[cards[cow][r] for cow in range(n)] for r in range(m)]
-    orders = []
-    for r in range(m):
-        order = sorted(range(n), key=lambda i: rounds[r][i])
-        orders.append(order)
-    if all(orders[r] == orders[0] for r in range(m)):
-        print(' '.join(str(i + 1) for i in orders[0]))
-    else:
-        print(-1)
+
+def solve():
+    t = int(input())
+    for _ in range(t):
+        n, m = map(int, input().split())
+        cows = []
+        card_to_cow = {}
+        for cow_idx in range(n):
+            cards = list(map(int, input().split()))
+            cows.append(cards)
+            for card in cards:
+                card_to_cow[card] = cow_idx + 1 
+        
+        all_cards = sorted(card_to_cow.keys())
+        cow_pos_mod = {}
+        possible = True
+        for card in all_cards:
+            cow = card_to_cow[card]
+            pos = all_cards.index(card)
+            mod = pos % n
+            if cow in cow_pos_mod:
+                if cow_pos_mod[cow] != mod:
+                    possible = False
+                    break
+            else:
+                cow_pos_mod[cow] = mod
+        if not possible:
+            print(-1)
+            continue
+        
+        mod_to_cow = {}
+        for cow, mod in cow_pos_mod.items():
+            if mod in mod_to_cow:
+                possible = False
+                break
+            mod_to_cow[mod] = cow
+        if not possible or len(mod_to_cow) != n:
+            print(-1)
+            continue
+        
+        p = [0] * n
+        for mod in range(n):
+            p[mod] = mod_to_cow[mod]
+        print(' '.join(map(str, p)))
+
+solve()
