e80ff21ac68bbd78d2967923e2a1ba9d77d30677
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-05-25 22:13:43 +0600

    5/25/25

--- neo_escape.py
+++ neo_escape.py
@@ -1,25 +1,33 @@
 for _ in range(int(input())):
-    n=int(input())
+    n = int(input())
     a = list(map(int, input().split()))
-    ma = max(a)
-    pos = a.index(ma)
-    c=0
-    while ma==0:
-        c+=1
-        com=ma
-        for i in range(pos+1,n):
-            if a[i]>com:
+    pressed = [False] * n
+    c = 0
+    while not all(pressed):
+        ma = -1
+        pos = -1
+        for i in range(n):
+            if not pressed[i] and a[i] > ma:
+                ma = a[i]
+                pos = i
+        if ma == -1:
+            break
+        c += 1
+        com = ma
+        pressed[pos] = True
+        for i in range(pos + 1, n):
+            if pressed[i]:
+                continue
+            if a[i] > com:
                 break
-            elif a[i]<com:
-                com=a[i]
-                a[i] = 0
-        for i in range(pos-1,-1,-1):
-            if a[i]>com:
+            com = a[i]
+            pressed[i] = True
+        com = ma
+        for i in range(pos - 1, -1, -1):
+            if pressed[i]:
+                continue
+            if a[i] > com:
                 break
-            elif a[i]<com:
-                com=a[i]
-                a[i] = 0
-        print(a)
-        ma=max(a)
-
+            com = a[i]
+            pressed[i] = True
     print(c)
