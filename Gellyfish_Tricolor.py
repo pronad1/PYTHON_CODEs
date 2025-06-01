b1d14268e317dcc56d7ec2c2d17bdd52c95db23f
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-06-01 11:23:30 +0600

    6/1/25

--- Gellyfish_Tricolor.py
+++ Gellyfish_Tricolor.py
@@ -1,14 +1,13 @@
 for _ in range(int(input())):
-    a, b, c, d = map(int, input().split())
-    flower_knight_hits = (c + d - 1) // d
-    gelly_knight_hits = (d + c - 1) // c
-    # How many hits does Flower's knight need to kill Gellyfish?
-    flower_hits = (a + d - 1) // d
-    # How many hits does Gellyfish's knight need to kill Flower?
-    gelly_hits = (b + c - 1) // c
-
-    # If Gellyfish's knight can kill Flower's knight before Flower's knight can kill Gellyfish, Gellyfish wins
-    if c <= d:
+    a,b,c,d=map(int, input().split())
+    fl=min(a,c)
+    kn=min(b,d)
+    if fl>kn:
+        print("Gellyfish")
+        
+    elif  fl<kn:
         print("Flower")
+        
     else:
         print("Gellyfish")
+        
