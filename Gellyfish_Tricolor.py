bfd4528a9ebaf3a2424fcff28eb72283fc1f384e
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-06-01 09:10:23 +0600

    6/1/25

--- Gellyfish_Tricolor.py
+++ Gellyfish_Tricolor.py
@@ -1,2 +1,18 @@
 for _ in range(int(input())):
-    a,b,c,d=
+    a,b,c,d=map(int, input().split())
+    fl=min(a,b)
+    kn=min(c,d)
+    if fl<kn:
+        ma=max(a,b)
+        if ma==a:
+            print("Gellyfish")
+        else:
+            print("Flower")
+            
+    else:
+        ma=max(c,d)
+        if ma==c:
+            print("Gellyfish")
+            
+        else:
+            print("Flower")
