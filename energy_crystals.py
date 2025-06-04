0eceb9b71aa3eb72f0930be309cb78415547622c
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-06-04 12:50:49 +0600

    6/4/25

--- energy_crystals.py
+++ energy_crystals.py
@@ -1,11 +1,9 @@
 for _ in range(int(input())):
     x=int(input())
     c=0
-    a,b,c=0,0,0
-    while a<x:
+    a=[0,0,0]
+    while min(a)<x:
         c+=1
-        a=min(x,b*2+1)
-        a, b = b, a
-        b, c = c, b
-        
+        a.sort()
+        a[0]=a[1]*2+1
     print(c)
