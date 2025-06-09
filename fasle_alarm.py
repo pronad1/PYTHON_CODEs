7a9b0369bae08b017f992f94c405a1dcb7af8663
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-06-09 18:54:08 +0600

    6/9/25

--- fasle_alarm.py
+++ fasle_alarm.py
@@ -1,33 +1,17 @@
 for _ in range(int(input())):
-    n,x= map(int, input().split())
+    n, x = map(int, input().split())
     a = list(map(int, input().split()))
 
-    ff,f= 0,0
-    for i in range(n-1):
-        if f==0 and a[i]==1:
-            f=1
-            idx = i
-            while idx<n and a[idx] == 1:
-                    a[idx] = 0
-                    if a[idx+1] == 1:
-                        idx += 1
-                    else:
-                        break
+    l = a.index(1)
 
-            if a[n-1]==1:
-                idx= n-1
-                while idx >i and a[idx] == 1:
-                    a[idx] = 0
-                    if a[idx-1] == 1:
-                        idx -= 1
-                    else:
-                        break
-
-            
-        if f==1 and a[i]==1:
-            ff=1
-            print("NO")
-            break
+    a_rev = list(reversed(a))
     
-    if ff==0:
+    r = a_rev.index(1)
+    r = n - r
+    
+    tot=r-l
+
+    if tot <= x:
         print("YES")
+    else:
+        print("NO")
