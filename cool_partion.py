5e7e339d53299980d0e686d1e9cfcc558319dab8
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-06-10 10:35:57 +0600

    6/10/25

--- cool_partion.py
+++ cool_partion.py
@@ -3,12 +3,14 @@ for _ in range(int(input())):
     a = list(map(int, input().split()))
 
     mp = {}
-    
+
     for x in a:
         mp[x] = mp.get(x, 0) + 1
 
     counts = sorted(mp.values(), reverse=True)
+
     if counts[0] == 1 or counts[1] == 1:
         print(1)
+        
     else:
         print(max(counts[0], counts[1]))
